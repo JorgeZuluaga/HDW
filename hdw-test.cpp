d0fbfa9cd9e21a9be81020f9a41a75314fccde24
JorgeZ <zuluagajorge@gmail.com>
2012-05-24 09:57:29 -0500

    Cambios

--- hdw-test.cpp
+++ hdw-test.cpp
@@ -1,16 +1,65 @@
-#include <gsl/gsl_math.h>
-#include <gsl/gsl_errno.h>
-#include <gsl/gsl_odeiv.h>
-
 ////////////////////////////////////////////////////
-//CONFIGURATION FILE
+//PROGRAM
 ////////////////////////////////////////////////////
 #include <hdw.h>
 #include <conf.h>
 
-int HydroDWSystem(double t,const double y[],const double dydt[],void *param);
-
-int main(int argc,char *argv[])
+int main(int argc,char* argv[])
 {
-  printf("S = %e\n",S);
+  double parameters[3];
+  double t,y[4],yerr[4],dydt[4],dydto[4];
+
+  //PARAMETERS
+  parameters[0]=1.0;//L(adimensional)
+  parameters[1]=0.6;//Ac(adimensional)
+  parameters[2]=8000;//zc(meters)
+
+  //INITIAL VALUES
+  y[0]=2*Topt;//T
+  y[1]=0.01;//ac
+  y[2]=0.01;//aw
+  y[3]=0.01;//ab
+
+  //DERIVATIVES
+  HDW_System(0,y,dydt,parameters);
+  printf("dT/dt = %e\n",dydt[0]);
+  printf("dac/dt = %e\n",dydt[1]);
+  printf("daw/dt = %e\n",dydt[2]);
+  printf("dab/dt = %e\n",dydt[3]);
+
+  //LET'S INTEGRATE 
+  const gsl_odeiv_step_type *T=gsl_odeiv_step_rk4;
+  gsl_odeiv_step *s=gsl_odeiv_step_alloc(T,4);
+  gsl_odeiv_system sys;
+  sys.function=&HDW_System;
+  sys.jacobian=NULL;
+  sys.dimension=4;
+  sys.params=parameters;
+
+  //INITIAL CODITIONS
+  printf("Initial conditions:\n");
+  printf("\tT = %e\n",y[0]);
+  printf("\tac = %e\n",y[1]);
+  printf("\taw = %e\n",y[2]);
+  printf("\tab = %e\n",y[3]);
+
+  //t is in years
+  double dt=0.1;
+  FILE *fl=fopen("HDW.dat","w");
+  for(t=0;t<=5000;t+=dt){
+    gsl_odeiv_step_apply(s,t,dt,y,yerr,dydt,dydto,&sys);
+    dydt[0]=dydto[0];
+    dydt[1]=dydto[1];
+    dydt[2]=dydto[2];
+    dydt[3]=dydto[3];
+    /*
+    printf("t = %e\n",t);
+    printf("\tT = %e\n",y[0]);
+    printf("\tac = %e\n",y[1]);
+    printf("\taw = %e\n",y[2]);
+    printf("\tab = %e\n",y[3]);
+    */
+    fprintf(fl,"%e %e %e %e %e\n",t,y[0],y[1],y[2],y[3]);
+  }
+  fclose(fl);
 }
--- hdw.cpp
+++ hdw.cpp
@@ -16,7 +16,80 @@
 #include <hdw.h>
 #include <hdw.conf>
 
-int MinMax(double *x,double &min,double &max)
+int HDW_System(double t,const double y[],double dydt[],void *param)
 {
+  //FREE PARAMETERES
+  double* parameters=(double*)param;
+  double L=parameters[0];
+  double Ac=parameters[1];
+  double zc=parameters[2];
+  double As;
+
+  double Ts=y[0];
+  double aclouds=y[1];
+  double awhite=y[2];
+  double ablack=y[3];
+  
+  double xx;
+  xx = 1.0 - awhite - ablack;
+  As = xx*Agf + ablack*Ab + awhite*Aw;
+
+  //========================================
+  //ENERGY BALANCE
+  //========================================
+  //y0 = T
+  double NSW,ILW,OLW;
+  double Tc;
+
+  Tc=Ts+grad_T*zc;
+  NSW=S*L*((1-Ac)*aclouds+(1-aclouds))*(1-As);
+  ILW=sigma*Ec*aclouds*PowInt(Tc,4);
+  OLW=sigma*Es*PowInt(Ts,4);
+
+  dydt[0]=(1/cp)*(NSW+ILW-OLW);
+
+  //========================================
+  //CLOUD DYNAMICS
+  //========================================
+  //y1 = aclouds
+  double Tanual,I,a,EVP,E,P;
+
+  //EVAPORATION
+  if(Ts>277){
+    Tanual = Ts - 273.	;//(°C)
+    I = 12.*pow((Tanual/5.),1.5);
+    a = (6.7e-7)*PowInt(I,3) - (7.7e-5)*PowInt(I,2) + (1.8e-2)*I + 0.49;
+    EVP = 12.*16*pow((10.*(Ts - 273.)/I),a);
+    E = Min(1.,EVP/EVPmax);
+  }else{
+    E = 0.;
+  }
+  
+  //PRECIPITATION
+  P = (1./Pmax)*pow((aclouds/mm),(1./nn));
+
+  dydt[1]=(1-aclouds)*E-aclouds*P;
+
+  //========================================
+  //DAISY POPULATION DYNAMICS
+  //========================================
+  //y2 = awhite
+  //y3 = ablack
+  double Bw,Bb,Tlb,Tlw;
+
+  Tlw=q*(As-Aw)+Ts;
+  //GROW RATE
+  if((Tlw>278)&&(Tlw<313))
+    Bw=1-0.003265*PowInt((Topt-Tlw),2);
+  else Bw=0;
+
+  Tlb=q*(As-Ab)+Ts;
+  if((Tlb>278)&&(Tlb<313)) 
+    Bb=1-0.003265*PowInt((Topt-Tlb),2);
+  else Bb=0;
+
+  dydt[2]=awhite*(xx*Bw-Gamma);
+  dydt[3]=ablack*(xx*Bb-Gamma);
+
   return 0;
 }
